d88aea32779d84ca916b1a9ef680c9aae96e7ed9
Bruno Lovric <brunoolovric@gmail.com>
2021-03-17 20:48:15 +0100

    added for project

--- ex14/src/index.js
+++ ex14/src/index.js
@@ -1,19 +1,21 @@
 function myForLoop1() {
-    var evenNumbers = "";
+    var evenNumbers = [];
     for (var i = 0; i < 10; i += 2) {
-        console.log(i);
+        evenNumbers.push(i);
     }
-    return evenNumbers;
+    return evenNumbers.join(', ');
 }
 function myForLoop2() {
-    var evenInverseNumbers = "";
-    for (var i = 8; i > 0; i -= 2) {
-        console.log(i);
+    var evenInverseNumbers = [];
+    for (var i = 8; i >= 0; i -= 2) {
+        evenInverseNumbers.push(i);
     }
-    return evenInverseNumbers;
+    return evenInverseNumbers.join(', ');
 }
-console.log(evenNumbers());
-console.log(evenInverseNumbers());
+myForLoop1();
+myForLoop2();
+console.log(myForLoop1());
+console.log(myForLoop2());
 module.exports = {
     myForLoop1,
     myForLoop2
