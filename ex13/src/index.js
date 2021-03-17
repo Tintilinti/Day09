8540b1b59e7cbb9c138d57e1974a4253852ed844
Bruno Lovric <brunoolovric@gmail.com>
2021-03-17 09:56:45 +0100

    added while project

---
+++ ex13/src/index.js
@@ -0,0 +1,11 @@
+function inverseWhile() {
+    var fiveNumbers = "";
+    var i = 5;
+    while(i >= 0) {
+        console.log(i);
+        i--;
+    }
+    return fiveNumbers;
+}
+console.log(inverseWhile());
+module.exports = inverseWhile;
